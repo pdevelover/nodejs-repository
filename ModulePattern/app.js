38a77b0eb87d96fcbf47d3716a473f65d784bf54
Pedro <pedro_webdevelover@outlook.pt>
2018-06-10 14:51:01 +0100

    all until lesson 7

---
+++ ModulePattern/app.js
@@ -0,0 +1,6 @@
+var stuff =   require('./stuff');
+
+
+console.log(stuff.counter(['pedro' , 'liliana']));
+
+console.log(stuff.adder(2,2));
---
+++ function expression/app.js
@@ -0,0 +1,16 @@
+//normal function
+function sayHi(){
+
+  console.log('hello bits');
+
+}
+
+sayHi();
+
+// dunction expression
+
+var sayBye = function(){
+
+  console.log('Bye bits');
+
+};
